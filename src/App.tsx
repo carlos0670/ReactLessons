b72b7edb60c27110ec7ca43b102d313602a2f2e0
Carlos Silva <carlos0670@gmail.com>
2023-11-14 22:38:04 +0000

    alert excercice create

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,12 @@
-import ListGroup from "./components/ListGroup";
-
-const cities = [
-  "New York",
-  "San Francisco",
-  "Tokyo",
-  "Paris",
-  "Barcelona",
-  "Porto",
-];
-const colors = [
-  "Red",
-  "Blue",
-  "Green",
-  "Yellow",
-  "Orange",
-  "Purple",
-  "Pink",
-  "Brown",
-  "Black",
-  "White",
-  "Gray",
-  "Silver",
-  "Gold",
-  "Cyan",
-  "Magenta",
-  "Turquoise",
-  "Indigo",
-  "Lavender",
-  "Olive",
-  "Maroon",
-];
-
-const hundleShowValue = (item: string) => {
-  console.log(item);
-};
+import Alert from "./components/Alert";
 
 function App() {
   return (
     <div>
-      <ListGroup
-        items={cities}
-        heading="Cities"
-        onShowValue={hundleShowValue}
-      />
+      <Alert>
+        <h1>Hello</h1>
+        <span>World</span>
+      </Alert>
     </div>
   );
 }
